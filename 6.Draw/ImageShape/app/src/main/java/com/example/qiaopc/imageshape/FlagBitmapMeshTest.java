bacc42ec6802c94d807ed2b93ddc7fe11cf7dbf9
qiaopc <qiaopc@gionee.com>
2018-01-20 23:07:36 +0800

    绘图机制flagWaves

---
+++ 6.Draw/ImageShape/app/src/main/java/com/example/qiaopc/imageshape/FlagBitmapMeshTest.java
@@ -0,0 +1,13 @@
+package com.example.qiaopc.imageshape;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FlagBitmapMeshTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flag_bitmap_mesh_test);
+    }
+}
---
+++ 6.Draw/ImageShape/app/src/main/java/com/example/qiaopc/imageshape/ReflectViewTest.java
@@ -0,0 +1,13 @@
+package com.example.qiaopc.imageshape;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReflectViewTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reflect_view_test);
+    }
+}
