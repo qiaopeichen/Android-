622730e3cf6cfd6a28756d88da33ce675c1acfa1
qiaopc <qiaopc@gionee.com>
2018-01-30 23:07:44 +0800

    SurfaceView-SinView

---
+++ 6.Draw/SurfaceViewTest/app/src/main/java/com/qiaopc/surfaceviewtest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.qiaopc.surfaceviewtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new SinView(MainActivity.this));
+    }
+}
