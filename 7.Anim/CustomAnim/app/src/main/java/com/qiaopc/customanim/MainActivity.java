bcc987d399cb1f3b3e693f7ff9e1464c28f36e7b
qiaopc <qiaopc@gionee.com>
2018-01-31 14:57:23 +0800

    customAnim Y轴旋转按钮30度-插值器BounceInterpolator

---
+++ 7.Anim/CustomAnim/app/src/main/java/com/qiaopc/customanim/MainActivity.java
@@ -0,0 +1,20 @@
+package com.qiaopc.customanim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnAnim(View view) {
+        CustomAnim customAnim = new CustomAnim();
+        customAnim.setRotateY(30);
+        view.startAnimation(customAnim);
+    }
+}
