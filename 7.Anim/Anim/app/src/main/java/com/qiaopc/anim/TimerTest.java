ec85bf43ff8eb70756ddf1eff134c9bf86880297
qiaopc <908694753@qq.com>
2018-02-02 14:51:21 +0800

    动画特效-计时器动画

--- 7.Anim/Anim/app/src/main/java/com/qiaopc/anim/TimerTest.java
+++ 7.Anim/Anim/app/src/main/java/com/qiaopc/anim/TimerTest.java
@@ -1,7 +1,10 @@
 package com.qiaopc.anim;
 
+import android.animation.ValueAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class TimerTest extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class TimerTest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer_test);
+    }
+
+    public void tvTimer(final View view) {
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(0, 100);
+        valueAnimator.addUpdateListener(
+                new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        ((TextView)view).setText("$ " + animation.getAnimatedValue());
+                    }
+                }
+        );
+        valueAnimator.setDuration(3000);
+        valueAnimator.start();
     }
 }
