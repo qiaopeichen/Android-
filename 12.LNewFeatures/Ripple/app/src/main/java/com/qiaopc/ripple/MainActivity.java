ac295a6621e3da5c6e6938d0926b782440454b75
qiaopc <908694753@qq.com>
2018-02-10 13:40:21 +0800

    5.X新特性 Circular reveal

--- 12.LNewFeatures/Ripple/app/src/main/java/com/qiaopc/ripple/MainActivity.java
+++ 12.LNewFeatures/Ripple/app/src/main/java/com/qiaopc/ripple/MainActivity.java
@@ -1,13 +1,50 @@
 package com.qiaopc.ripple;
 
+import android.animation.Animator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.reveal_layout);
+        final View oval = findViewById(R.id.oval);
+        oval.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animator animator =
+                        ViewAnimationUtils.createCircularReveal(
+                                oval,
+                                oval.getWidth() / 2,
+                                oval.getHeight() / 2,
+                                oval.getWidth() / 2,
+                                0);
+                animator.setInterpolator(new AccelerateDecelerateInterpolator());
+                animator.setDuration(2000);
+                animator.start();
+            }
+        });
+
+        final View rect = findViewById(R.id.rect);
+        rect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animator animator = ViewAnimationUtils.createCircularReveal(
+                        rect,
+                        0,
+                        0,
+                        0,
+                        (float) Math.hypot(rect.getWidth(), rect.getHeight()));
+                animator.setInterpolator(new AccelerateInterpolator());
+                animator.setDuration(2000);
+                animator.start();
+            }
+        });
     }
 }
